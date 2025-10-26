752b999599c74438a1c6f44529ebd6705d2c615c
Pradeep <deeppradeep1998@gmail.com>
2025-10-26 12:47:57 +0530

    first commit

--- 1.py
+++ 1.py
@@ -1,3 +1,7 @@
 numbers=[1,2,3,4,5,6,7,8,9,10]
 squared_numbers=[num**2 for num in numbers]
 print(squared_numbers)
+
+# Output: [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# This code creates a list of squared numbers from 1 to 10 using list comprehension.    
+# It iterates through each number in the original list 'numbers', squares it, and collects the results in 'squared_numbers'.
---
+++ 2.py
@@ -0,0 +1,8 @@
+c='12'
+c+='8'
+
+print(int(c)-3)
+
+# Output: 125
+# This code snippet initializes a string variable 'c' with the value '12', appends '8' to it, resulting in '128'.
+# It then converts the concatenated string to an integer and subtracts 3, yielding the final output of 125.
