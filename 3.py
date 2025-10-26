b2255fd0f96cc93c7a467d50aa9eb7ab5021090a
Pradeep <deeppradeep1998@gmail.com>
2025-10-26 16:09:14 +0530

    first commit

---
+++ 3.py
@@ -0,0 +1,7 @@
+d={'a':1,'b':2,'c':3}
+for k in reversed(list(d.keys())):
+    print(k,end='')
+
+# Output: cba
+# This code snippet initializes a dictionary 'd' with three key-value pairs.
+# It then iterates over the keys of the dictionary in reverse order and prints them without any spaces or newlines, resulting in the output 'cba'.
